36ffa96ab2deac60ea6936ab734723cb3259cc4f
epmoreno <enriquepinazomoreno20001@hotmail.com>
2023-03-21 19:48:20 +0100

    Ejercicio 5

--- Segundo semestre/MP/Segunda_entrega/Ejercicio5.cpp
+++ Segundo semestre/MP/Segunda_entrega/Ejercicio5.cpp
@@ -16,14 +16,15 @@ int main(){
    Course *r_MP[] = {&Estructuras_datos};
    Course Metodologia_programacion = {"MP",r_MP,1};
 
-   Course *r_FP[] = {&Metodologia_programacion};
-   Course Fundamento_programacion = {"FP",r_FP,1};
+   Course *r_FP[] = {&Metodologia_programacion,&Estructuras_datos};
+   Course Fundamento_programacion = {"FP",r_FP,2};
 
    cout << Fundamento_programacion.name << " tiene " << Fundamento_programacion.num_requisites << " requisitos:" <<endl;
    for (int i = 0; i < Fundamento_programacion.num_requisites; i++)
    {
         cout << "- " << Fundamento_programacion.requisites[i]->name << endl;
    }
+   cout << Calculo.name << " tiene " << Calculo.num_requisites << " requisitos:" <<endl;
+
    return 0;
-   
-};
+}
