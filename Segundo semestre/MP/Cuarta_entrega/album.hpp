3b9531c0848d6af49b6b8d97f2ae508d2b188e07
epmoreno <enriquepinazomoreno20001@hotmail.com>
2023-05-29 21:39:31 +0200

    1º_1_1_Spotifry_Album_Class
    Clase "Album" terminada.

---
+++ Segundo semestre/MP/Cuarta_entrega/album.hpp
@@ -0,0 +1,66 @@
+#ifndef ALBUM_HPP
+#define ALBUM_HPP
+
+/**
+ * ?CLASES A AÑADIR?
+ * *#include "date.hpp"
+ * *#include "song.hpp"
+ * *#include "artist.hpp"
+ **/
+#include <string>
+
+class Album{
+    private:
+        std::string title;
+        Artist* artist;
+        Date releaseDate;
+        int num_songs;
+        Song** songs;
+    public:
+        Album();
+        Album(std::string title, Artist* artist, Date releaseDate, Song** songs, int num_songs);
+        ~Album();
+
+        
+        /** @param Gets_Y_Sets **/
+
+        std::string getTitle() { return title; }
+        void setTitle(std::string title) { this->title = title; }
+
+        Date getReleaseDate() { return releaseDate; }
+        void setReleaseDate(Date releaseDate) {this->releaseDate = releaseDate; }
+
+        int getNum_songs() { return num_songs; }
+        void setNum_songs(int num_songs) { this->num_songs = num_songs; }
+
+        Artist* getArtist() { return artist; }
+        void setArtist(Artist* artist) { this->artist = artist; }
+};
+
+Album::Album(){
+    /**
+     * ?POR DEFECTO.
+    */
+}
+
+Album::Album(std::string title, Artist* artist, Date releaseDate, Song** songs, int num_songs){
+    this->title = title;
+    this->artist = artist;
+    this->releaseDate = releaseDate;
+    this->songs = new Song*[num_songs];
+    this->num_songs = num_songs;
+    for (int i = 0; i < num_songs; i++){ //*Con este bucle añadiremos las conciones a Song
+        this->songs[i] = songs[i];
+    }
+    
+}
+
+/**
+ * ?DESTRUCTOR M_D
+*/
+Album::~Album(){
+    delete artist;
+    delete[] songs;
+}
+
+#endif
